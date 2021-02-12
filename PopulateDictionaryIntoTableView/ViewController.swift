1107dc27d9ddeb558afd75ea4d2138ac06ff9437
Rosy <rosyranipatel123@gmail.com>
2021-02-12 16:18:26 +0000

    Doing first commit

--- PopulateDictionaryIntoTableView/ViewController.swift
+++ PopulateDictionaryIntoTableView/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var myDictionary: [String:String] = ["Chocolate Chip": "0.25","Oatmeal": "0.26",
+                                         "Peanut Butter": "0.02", "Sugar": "0.03",
+                                         "VanilaExtract": "0.01","CocoPowder": "1",
+                                         "b": "2", "c": "3","d": "4","e": "5"]
+    //var valuesArray = [String]()
+    //let keyArray = [String](myDictionary.keys)
+   
+    
+    var valueArray: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for myKey in myDictionary.keys{
+            
+            valueArray.append(myKey)
+        }
+        
+        tableViewOutlet.dataSource = self
+        tableViewOutlet.delegate = self
+        
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        myDictionary.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        cell.textLabel?.text = valueArray[indexPath.row]
+        return cell
+    }
 
 }
 
